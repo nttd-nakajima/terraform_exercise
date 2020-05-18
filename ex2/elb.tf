5a6e6d42e7252160dd614c10ea0880deaf297b0b
Kazuki Nakajima <kazuki.nakajima@fastretailing.com>
2020-05-18 10:51:18 +0900

    fmt

--- ex2/elb.tf
+++ ex2/elb.tf
@@ -9,7 +9,7 @@ resource "aws_alb" "web" {
   enable_deletion_protection = false
 
   tags = {
-      Name = "ex2-alb"
+    Name = "ex2-alb"
   }
 }
 
@@ -17,46 +17,46 @@ resource "aws_alb" "web" {
 # ALB - Listener
 # ===========================
 resource "aws_alb_listener" "web" {
-    load_balancer_arn = aws_alb.web.arn
-    port = "80"
-    protocol = "HTTP"
+  load_balancer_arn = aws_alb.web.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-    default_action {
-        type = "forward"
-        target_group_arn = aws_alb_target_group.web.arn
-    }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.web.arn
+  }
 }
 
 resource "aws_lb_listener_rule" "forward" {
-    listener_arn = aws_alb_listener.web.arn
-    priority = 99
+  listener_arn = aws_alb_listener.web.arn
+  priority     = 99
 
-    action {
-        type = "forward"
-        target_group_arn = aws_alb_target_group.web.arn
-    }
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.web.arn
+  }
 
-    condition {
-        path_pattern {
-            values = ["/*"]
-        }
+  condition {
+    path_pattern {
+      values = ["/*"]
     }
+  }
 }
 
 resource "aws_alb_target_group" "web" {
-    name = "ex2-web-alb-tg"
-    port = 80
-    protocol = "HTTP"
-    vpc_id = aws_vpc.vpc.id
+  name     = "ex2-web-alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
 
-    health_check {
-        path = "/index.html"
-    }
+  health_check {
+    path = "/index.html"
+  }
 }
 
 resource "aws_lb_target_group_attachment" "web" {
-    count = 2
-    target_group_arn = element(aws_alb_target_group.web.*.arn, count.index%2)
-    target_id = element(aws_instance.web.*.id, count.index%2)
-    port = 80
+  count            = 2
+  target_group_arn = element(aws_alb_target_group.web.*.arn, count.index % 2)
+  target_id        = element(aws_instance.web.*.id, count.index % 2)
+  port             = 80
 }
